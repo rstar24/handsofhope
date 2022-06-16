c1e24313b54e6a06e102a04afa7b5d2bef883a54
Harsh Rathod <hrathore50@ymail.com>
2022-06-16 12:25:32 +0530

    Integrated the Roboto font-family.

--- src/cyfwms-twn-client/src/index.tsx
+++ src/cyfwms-twn-client/src/index.tsx
@@ -6,6 +6,10 @@ import { store } from "./app/store";
 import Home from "./pages/Home";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./index.css";
 
 const container = document.getElementById("root")!;
