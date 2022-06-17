b13b0e811dafde7ca229145c65112a2c4af54ce7
Harsh Rathod <hrathore50@ymail.com>
2022-06-17 14:27:07 +0530

    Added theme color palette.

--- src/cyfwms-twn-client/src/lib/theme.ts
+++ src/cyfwms-twn-client/src/lib/theme.ts
@@ -1,5 +1,12 @@
 import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#da0404",
+      dark: "#770c0c",
+    },
+  },
+});
 
 export default theme;
