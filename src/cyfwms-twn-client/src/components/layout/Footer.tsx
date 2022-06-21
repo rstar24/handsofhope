96f07a53d7e30d953973798ede7591fa9f82a6ab
Harsh Rathod <hrathore50@ymail.com>
2022-06-22 00:47:12 +0530

    Converted bottom.png to bottom.svg file and integrated it.

--- src/cyfwms-twn-client/src/components/layout/Footer.tsx
+++ src/cyfwms-twn-client/src/components/layout/Footer.tsx
@@ -9,13 +9,9 @@ const Footer = (): ReactElement => {
   return (
     <Box>
       <Box
-        sx={{
-          backgroundImage: "url('/img/bottom.png')",
-          backgroundPosition: "top center",
-          backgroundSize: { xs: "960px 100%", md: "100% 100%" },
-          height: "68px",
-          width: "100%",
-        }}
+        component="img"
+        src="/img/bottom.svg"
+        sx={{ display: "block", width: "100%" }}
       />
     </Box>
   );
