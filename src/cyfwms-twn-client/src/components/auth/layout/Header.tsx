4f25ef07f701e73df89f3021851015518f504c13
Harsh Rathod <hrathore50@ymail.com>
2022-06-21 17:19:47 +0530

    Designed and implemented /home page.

---
+++ src/cyfwms-twn-client/src/components/auth/layout/Header.tsx
@@ -0,0 +1,33 @@
+import { Box, Typography } from "@mui/material";
+import type { ReactElement } from "react";
+
+/**
+ * The Header functional component.
+ * @returns Header component skeleton.
+ */
+const Header = (): ReactElement => {
+  return (
+    <Box
+      sx={{
+        borderBottom: "5px solid black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h5" component="h1">
+        Child, Youth, and Family Services
+      </Typography>
+      <Box
+        component="img"
+        src="/img/mascot.png"
+        sx={{ mx: "1rem", mb: "1rem", width: "100px" }}
+      />
+      <Typography variant="h5" component="h1">
+        Wellness Management System
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
---
+++ src/cyfwms-twn-client/src/components/auth/layout/Layout.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+import Footer from "../../layout/Footer";
+import Header from "./Header";
+import type { ReactChildren } from "../../../shared/types";
+import type { ReactElement } from "react";
+
+/**
+ * The Layout functional component.
+ * Will be visible just to logged-in/authorized user.
+ * @returns Layout component skeleton.
+ */
+export const Layout = (props: ReactChildren): ReactElement => {
+  return (
+    <Box
+      sx={{
+        border: "5px solid black",
+        mx: { xs: "", md: "5rem" },
+        my: { xs: "", md: "1rem" },
+      }}
+    >
+      <Header />
+      {props.children}
+      <Footer />
+    </Box>
+  );
+};
