d0f760b6adc37815a48d9a7fdd4b5cb4b9f420e0
Harsh Rathod <hrathore50@ymail.com>
2022-06-24 20:19:47 +0530

    Extracted out app related material from index.tsx to CYFSWMSApp.tsx file.

--- src/cyfwms-twn-client/src/index.tsx
+++ src/cyfwms-twn-client/src/index.tsx
@@ -1,49 +1,13 @@
-import { ThemeProvider } from "@mui/material/styles";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { store } from "./app/store";
-import theme from "./lib/theme";
-import CYFMS from "./pages/cyfms/CYFMS";
-import CYFMSSearch from "./pages/cyfms/CYFMSSearch";
-import CYFMSRegister from "./pages/cyfms/CYFMSRegister";
-import Other from "./pages/cyfms/Other";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import NotFound404 from "./pages/NotFound404";
-import reportWebVitals from "./reportWebVitals";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import "./index.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CYFSWMSApp from "./CYFSWMSApp";
+import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<NotFound404 />} />
-            <Route path="/" element={<Navigate to="login" />} />
-            <Route path="home" element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="cyfms">
-              <Route path="" element={<CYFMS />} />
-              <Route path="search" element={<CYFMSSearch />} />
-              <Route path="register" element={<CYFMSRegister />} />
-              <Route path="other" element={<Other />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
-  </StrictMode>
-);
+root.render(<CYFSWMSApp />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
