f1143fa62fa95b1c727fd71a93359ead9d061587
Anupam <anupam.mishra@gigatorb.com>
2022-06-29 17:55:24 +0530

    Other Information API Integration in frontend

---
+++ src/cyfwms-twn-client/src/features/otherInformation/otherInformationAPI.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import { OtherInformationGetData, OtherInformationPostData } from "./otherInformationSlice";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:9088/v1/participantservice/",
+});
+
+export const doGetOtherInformationAPI = async (
+  data: OtherInformationGetData,
+  jwtToken: string
+): Promise<AxiosResponse> => {
+  console.log(data);
+  const res: AxiosResponse = await axiosInstance.get(
+    "readParticipantOtherInformation/" + data,
+    {
+      headers: { Authorization: "Bearer " + jwtToken },
+    }
+  );
+  return res;
+};
+
+export const doPostOtherInformationAPI = async (
+  data: OtherInformationPostData,
+  jwtToken: string
+): Promise<AxiosResponse> => {
+  console.log(data);
+  const res: AxiosResponse = await axiosInstance.put(
+    "saveParticipantOtherInformation/",
+    data.user,
+    {
+      headers: { Authorization: "Bearer " + jwtToken },
+    }
+  );
+  return res;
+};
