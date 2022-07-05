f95c610426fb2724c9aa371956323370d38a9b92
Nazim Khan <nazimkhan71999@gmail.com>
2022-07-05 10:33:11 +0530

    Added CYFMSInput1 file for password show and hide as well as Added active link functionality

--- src/cyfwms-twn-client/src/components/cyfms/CYFMSSideNav.tsx
+++ src/cyfwms-twn-client/src/components/cyfms/CYFMSSideNav.tsx
@@ -1,9 +1,14 @@
 import { useAppSelector } from "../../library/hooks";
 import { Box, List, ListItemButton, ListItemText } from "@mui/material";
 import { createSelector } from "@reduxjs/toolkit";
+import { NavLink } from "react-router-dom";
 import React from "react";
-import { Link } from "react-router-dom";
+import ListItem, { listItemClasses } from "@mui/material/ListItem";
 import type { ReactElement } from "react";
+
+function MyNavLink(props: any) {
+  return <NavLink {...props} activeClassName="active" />;
+}
 
 const hideTabsSelector = createSelector(
   (state: any) => (state as any).cyfmsSideNav.hideTabs,
@@ -19,55 +24,64 @@ const CYFMSSideNav = (): ReactElement => {
 
   return (
     <Box aria-label="tabs" sx={{ bgcolor: "background.paper" }}>
-      <List disablePadding>
-        <ListItemButton component={Link} to="/cyfms/register">
+      <List
+        disablePadding
+        sx={{
+          [`& .active, & .${listItemClasses.root}:hover`]: {
+            color: "red",
+            backgroundColor: "#D9D9D9",
+          },
+        }}
+      >
+        <ListItemButton component={MyNavLink} to="/cyfms/register">
           <ListItemText primary="Identity" />
         </ListItemButton>
+
         <ListItemButton
           disabled={hideTabs}
-          component={Link}
+          component={MyNavLink}
           to="/cyfms/contact"
         >
           <ListItemText primary="Contact" />
         </ListItemButton>
         <ListItemButton
           disabled={hideTabs}
-          component={Link}
+          component={MyNavLink}
           to="/cyfms/household_members"
         >
           <ListItemText primary="Household Members" />
         </ListItemButton>
         <ListItemButton
           disabled={hideTabs}
-          component={Link}
+          component={MyNavLink}
           to="/cyfms/education_and_employment"
         >
           <ListItemText primary="Education and Employment" />
         </ListItemButton>
         <ListItemButton
           disabled={hideTabs}
-          component={Link}
+          component={MyNavLink}
           to="/cyfms/criminal_history"
         >
           <ListItemText primary="Criminal History" />
         </ListItemButton>
         <ListItemButton
           disabled={hideTabs}
-          component={Link}
+          component={MyNavLink}
           to="/cyfms/family_physician"
         >
           <ListItemText primary="Family Physician" />
         </ListItemButton>
         <ListItemButton
           disabled={hideTabs}
-          component={Link}
+          component={MyNavLink}
           to="/cyfms/cyfms_worker"
         >
           <ListItemText primary="Conselor / CYFMS Worker" />
         </ListItemButton>
         <ListItemButton
           disabled={hideTabs}
-          component={Link}
+          component={MyNavLink}
           to="/cyfms/other_information"
         >
           <ListItemText primary="Other Information" />
--- src/cyfwms-twn-client/src/pages/Login.tsx
+++ src/cyfwms-twn-client/src/pages/Login.tsx
@@ -14,6 +14,7 @@ import Layout from "../components/layout/Layout";
 import React, { useState } from "react";
 import type { FormEvent, ReactElement } from "react";
 import CYFMSInput from "../components/cyfms/CYFMSInput";
+import CYFMSInput1 from "../components/cyfms/CYFMSInput1";
 
 const style = {
   position: "absolute",
@@ -103,7 +104,7 @@ const Login = (): ReactElement => {
         }}
       >
         <CYFMSInput id="userName" name="userName" required value="Username" />
-        <CYFMSInput
+        <CYFMSInput1
           id="passWord"
           name="passWord"
           required
