9974ef114f20f513978d7e7b1ae66412fe331840
Ankit Kushwah <ankit.kushwah@gigatorb.com>
2022-08-29 15:12:03 +0530

    remove field status of criminalHistory

--- src/main/java/com/twn/cyfwms/participant/repository/CriminalHistoryRepository.java
+++ src/main/java/com/twn/cyfwms/participant/repository/CriminalHistoryRepository.java
@@ -2,11 +2,9 @@ package com.twn.cyfwms.participant.repository;
 
 import com.twn.cyfwms.participant.entity.CriminalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CriminalHistoryRepository extends JpaRepository<CriminalHistory, Long> {
-    @Query(value = "select * from criminalhistory where participantId=? AND status='ACTIVE'",nativeQuery = true)
     CriminalHistory findByParticipantId(Long participantId);
 }
