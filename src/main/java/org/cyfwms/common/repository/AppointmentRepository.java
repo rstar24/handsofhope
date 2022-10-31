3e3d29ee65619072ad9ca002fd8451bcc7331c8f
Nazim Khan <nazimkhan71999@gmail.com>
2022-10-31 11:41:43 +0530

    Added Appointment Repository

---
+++ src/main/java/org/cyfwms/common/repository/AppointmentRepository.java
@@ -0,0 +1,17 @@
+package org.cyfwms.common.repository;
+
+import org.cyfwms.common.entity.Appointments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointments,Long> {
+    @Query(value = "select * from appointments where appointmentid=? AND appointmentStatus ='Active'",nativeQuery = true)
+    Appointments findByAppointmentId(Long appointmentIdId);
+    @Query(value = "select * from appointments where date=? AND appointmentStatus ='Active' AND status='sheduled'",nativeQuery = true)
+    List<Appointments> findByDate(LocalDate date);
+}
